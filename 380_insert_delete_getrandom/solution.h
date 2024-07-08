668c142db6592375597820b33cfebee4c3451861
Sergey Shambir <sergey.shambir@ispring.ru>
2024-07-08 19:44:06 +0300

    380. Insert Delete GetRandom O(1) Реализовал корректное решение (с подсказкой по способу удаления элемента)

--- 380_insert_delete_getrandom/solution.h
+++ 380_insert_delete_getrandom/solution.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <memory>
 #include <random>
-#include <stdexcept>
-#include <utility>
+#include <unordered_map>
+#include <vector>
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
@@ -17,135 +16,48 @@ class RandomizedSet
 {
 public:
     RandomizedSet()
-        : state(new State(8, std::random_device()()))
+        : generator(std::random_device()())
     {
     }
 
     bool insert(int val)
     {
-        rehashIfNeeded();
-        return state->insert(val);
+        if (indexMap.contains(val))
+        {
+            return false;
+        }
+        values.push_back(val);
+        indexMap[val] = values.size() - 1;
+        return true;
     }
 
     bool remove(int val)
     {
-        return state->remove(val);
+        const auto it = indexMap.find(val);
+        if (it != indexMap.end())
+        {
+            const size_t foundIndex = it->second;
+            const size_t lastIndex = values.size() - 1;
+
+            std::swap(values[foundIndex], values[lastIndex]);
+            indexMap[values[foundIndex]] = foundIndex;
+
+            indexMap.erase(val);
+            values.pop_back();
+
+            return true;
+        }
+        return false;
     }
 
     int getRandom()
     {
-        return state->getRandom();
+        std::uniform_int_distribution<size_t> distribution(0, values.size() - 1);
+        return values[distribution(generator)];
     }
 
 private:
-    void rehashIfNeeded()
-    {
-        if (2 * state->usedItemsCount < state->items.size())
-        {
-            return;
-        }
-        std::unique_ptr<State> newState(new State(state->items.size() * 2, 0));
-        for (const Item& item : state->items)
-        {
-            if (item.state == ItemState::Used)
-            {
-                newState->insert(item.value);
-            }
-        }
-        std::swap(state, newState);
-    }
-
-    enum class ItemState
-    {
-        Unused,
-        Used,
-        Erased,
-    };
-
-    struct Item
-    {
-        ItemState state = ItemState::Unused;
-        int value = 0;
-    };
-
-    struct State
-    {
-        size_t usedItemsCount = 0;
-        std::vector<Item> items;
-        std::mt19937 generator;
-        std::uniform_int_distribution<> distribution;
-
-        State(size_t size, std::random_device::result_type seed)
-            : items(size, Item{})
-            , generator(seed)
-            , distribution(0, size - 1)
-        {
-        }
-
-        bool insert(int val)
-        {
-            size_t size = items.size();
-            size_t position = std::hash<int>{}(val) % size;
-            size_t i = position;
-            do
-            {
-                if (items[i].state != ItemState::Used)
-                {
-                    items[i] = Item{ ItemState::Used, val };
-                    ++usedItemsCount;
-                    return true;
-                }
-                if (items[i].state == ItemState::Used && items[i].value == val)
-                {
-                    return false;
-                }
-
-                i = (i + 1) % size;
-            } while (i != position);
-
-            throw std::logic_error("cannot insert: hash table is full");
-        }
-
-        bool remove(int val)
-        {
-            size_t size = items.size();
-            size_t position = std::hash<int>{}(val) % size;
-            size_t i = position;
-            do
-            {
-                if (items[i].state == ItemState::Unused)
-                {
-                    return false;
-                }
-                if (items[i].state == ItemState::Used && items[i].value == val)
-                {
-                    items[i].state = ItemState::Erased;
-                    --usedItemsCount;
-                    return true;
-                }
-                i = (i + 1) % size;
-            } while (i != position);
-
-            return false;
-        }
-
-        int getRandom()
-        {
-            size_t size = items.size();
-            size_t position = distribution(generator);
-            size_t i = position;
-            do
-            {
-                if (items[i].state == ItemState::Used)
-                {
-                    return items[i].value;
-                }
-                i = (i + 1) % size;
-            } while (i != position);
-
-            throw std::logic_error("cannot find random value: hash table is empty");
-        }
-    };
-
-    std::unique_ptr<State> state;
+    std::unordered_map<int, size_t> indexMap;
+    std::vector<int> values;
+    std::mt19937 generator;
 };
