e60c986ee7666abf859aeec1206b85e0f86042e3
Sergey Shambir <sergey.shambir@ispring.ru>
2024-07-10 22:57:30 +0300

    15. 3Sum Оптимизировал решение (без улучшения алгоритмической сложности)

--- 15_3sum/solution.h
+++ 15_3sum/solution.h
@@ -1,35 +1,44 @@
 #pragma once
 
-#include <string>
-#include <vector>
 #include <algorithm>
 #include <set>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
         // Sort to use binary search.
         sort(nums.begin(), nums.end());
 
         set<vector<int>> results;
+        vector<int> nextResult(3, 0);
 
-        const int *pEnd = nums.data() + nums.size(); 
-        for (const int *p1 = nums.data(); p1 < pEnd; ++p1)
+        const int* pEnd = nums.data() + nums.size();
+        for (const int* p1 = nums.data(); p1 < pEnd; ++p1)
         {
-            for (const int *p2 = p1 + 1; p2 < pEnd; ++p2)
+            nextResult[0] = *p1;
+            for (const int* p2 = p1 + 1; p2 < pEnd; ++p2)
             {
+                nextResult[1] = *p2;
+
                 const int needle = -(*p1 + *p2);
-                const auto it = std::lower_bound(p2+1, pEnd, needle);
+                const auto it = std::lower_bound(p2 + 1, pEnd, needle);
                 if (it != pEnd && *it == needle)
                 {
-                    vector<int> result{*p1, *p2, needle};
-                    results.insert(result);
+                    nextResult[2] = needle;
+                    if (!results.contains(nextResult))
+                    {
+                        results.insert(nextResult);
+                    }
                 }
             }
         }
-        
+
         return vector<vector<int>>(results.begin(), results.end());
     }
 };
